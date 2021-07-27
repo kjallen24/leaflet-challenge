30af5d1469d35d807ed6961eddfd7400dd5b9c96
kallen <kadatagirl@gmail.com>
2021-07-27 15:15:50 -0500

    first

--- leafleft-step1/js/code.js
+++ leafleft-step1/js/code.js
@@ -23,7 +23,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geoj
      weight: 0.5
    };
  }
- // Gradient color scale for magnitude markers
+ // Gradient color scale for magnitude markersm using getColor func
  function getColor(magnitude) {
     switch (true) {
       case magnitude > 5:
